0902dfbcf125fa565f88454834c5cfdab362e52e
imtiaj2018 <imtiajromi@gmail.com>
2020-09-20 16:26:22 +0530

    user panel

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  skip_before_action :require_no_authentication, only: [:new]
+  skip_before_action :require_no_authentication, only: [:new, :create]
 
   # GET /resource/sign_up
   def new
@@ -11,9 +11,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
   # def edit
