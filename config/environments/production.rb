917cd1d1d9ab3f5b62f7796d6e83fd01d4fe5f27
imtiaj2018 <imtiajromi@gmail.com>
2020-10-22 00:40:17 +0530

    design changed1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
 	config.active_record.dump_schema_after_migration = false
-  # config.action_mailer.default_url_options = { host: 'http://sunshineadsolutions.com' }
-	config.action_mailer.default_url_options = { host: 'sunshineadsolutions.com' }
+  # config.action_mailer.default_url_options = { host: 'https://sunshineadsolutions.herokuapp.com' }
+	config.action_mailer.default_url_options = { host: 'sunshineadsolutions.herokuapp.com' }
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.delivery_method = :smtp 
 	config.action_mailer.raise_delivery_errors = true 
