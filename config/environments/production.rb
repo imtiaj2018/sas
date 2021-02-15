53b03c3ec774fe961c7a86fcf1675886b0c76823
imtiaj2018 <imtiajromi@gmail.com>
2021-02-15 20:32:27 +0530

    mail fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
 	config.active_record.dump_schema_after_migration = false
     # config.action_mailer.default_url_options = { host: 'https://sunshineadsolutions.herokuapp.com' }
-	config.action_mailer.default_url_options = { host: 'sunshineadsolutions.herokuapp.com' }
+	config.action_mailer.default_url_options = { host: 'sunshineadsolutions234567.herokuapp.com' }
 	# config.action_mailer.default_url_options = { host: 'http://www.sunshineadsolutions.com' }
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.delivery_method = :smtp 
