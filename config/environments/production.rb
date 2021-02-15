9b37cb433b0d3018df2c33907a1c8d2650299ae8
imtiaj2018 <imtiajromi@gmail.com>
2021-02-15 21:27:00 +0530

    mail fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,19 +91,29 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
 	config.active_record.dump_schema_after_migration = false
-    # config.action_mailer.default_url_options = { host: 'https://sunshineadsolutions.herokuapp.com' }
-	config.action_mailer.default_url_options = { host: 'sunshineadsolutions.herokuapp.com' }
+  # # config.action_mailer.default_url_options = { host: 'https://sunshineadsolutions.herokuapp.com' }
+	# # config.action_mailer.default_url_options = { host: 'sunshineadsolutions.herokuapp.com' }
 	# config.action_mailer.default_url_options = { host: 'http://www.sunshineadsolutions.com' }
-	config.action_mailer.perform_deliveries = true
-	config.action_mailer.delivery_method = :smtp 
-	config.action_mailer.raise_delivery_errors = true 
-	config.action_mailer.smtp_settings = {
-		user_name:      'sunshineadsolutions@gmail.com',
-		password:       'kolkatA@2020business',
-		domain:         'gmail.com',
-		address:       'smtp.gmail.com',
-		port:          '587',
-		authentication: :plain,
-		enable_starttls_auto: true
-	}
+	# config.action_mailer.perform_deliveries = true
+	# config.action_mailer.delivery_method = :smtp 
+	# config.action_mailer.raise_delivery_errors = true 
+	# config.action_mailer.smtp_settings = {
+		# user_name:      'sunshineadsolutions@gmail.com',
+		# password:       'kolkatA@2020business',
+		# domain:         'gmail.com',
+		# address:       'smtp.gmail.com',
+		# port:          '25',
+		# authentication: :login,
+		# enable_starttls_auto: true
+	# }
+	
+	config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+		:port           => ENV['MAILGUN_SMTP_PORT'],
+		:address        => ENV['MAILGUN_SMTP_SERVER'],
+		:user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+		:password       => ENV['MAILGUN_SMTP_PASSWORD'],
+		:domain         => 'sunshineadsolutions.herokuapp.com', #mydomain actually contains the realvalue
+		:authentication => :plain,
+    }
 end
