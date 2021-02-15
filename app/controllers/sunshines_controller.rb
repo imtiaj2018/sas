4b13d668a5b662081efff9671adfa0c94439cd83
imtiaj2018 <imtiajromi@gmail.com>
2021-02-15 20:23:22 +0530

    mail fixed

--- app/controllers/sunshines_controller.rb
+++ app/controllers/sunshines_controller.rb
@@ -48,6 +48,8 @@ class SunshinesController < ApplicationController
 		sunshine_message = "<br></br>Thank you for contact us.<br> We will get back to you soon<br>"
 		message = "<br>Hi Sunshine Team,</br><br><br> Visitor Query: #{message}</br><br><br> Visitor Email: #{recipient}</br>#{visitor_phone_str}"
 		# UserMailer.send_mail(sunshine_mail, sunshine_subject, sunshine_message).deliver
+		Rails.logger.info "========================******===========#{recipient}=========="
+		puts "========================******===========#{recipient}=========="
 		UserMailer.send_mail(recipient, subject, message).deliver
 		# redirect_to '/register_by_mail?status=Message sent successfully'
 		# redirect_to '/home'
