1cf1fd817d4f2800125bc7d817c2334d403aafbe
imtiaj2018 <imtiajromi@gmail.com>
2021-02-18 00:23:50 +0530

    mailgun implementation677677

--- app/controllers/sunshines_controller.rb
+++ app/controllers/sunshines_controller.rb
@@ -46,8 +46,10 @@ class SunshinesController < ApplicationController
 		recipient_message = "<br></br>Thank you for contact us.<br> We will get back to you soon<br>"
 		sunshine_message = "<br>Hi Sunshine Team,</br><br><br> Visitor Query: #{message}</br><br><br> Visitor Email: #{recipient_mail}</br>#{visitor_phone_str}"
 		
+		Rails.logger.info "====im annn"
 		UserMailer.send_mail(recipient_mail, recipient_subject, recipient_message).deliver #send to the visiter
-		# UserMailer.send_mail(sunshine_mail, sunshine_subject, sunshine_message).deliver #send to sunshine official mail id
+		UserMailer.send_mail(sunshine_mail, sunshine_subject, sunshine_message).deliver #send to sunshine official mail id
+		
 		# redirect_to '/register_by_mail?status=Message sent successfully'
 		# redirect_to '/home'
 	end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,6 @@ class UserMailer < ApplicationMailer
 	default from: 'sunshineadsolutions@gmail.com'
 	
 	def send_mail(user_email, subject, message) 
-		Rails.logger.info "========================1==========="
-		Rails.logger.info "============#{user_email}======2================="
-		Rails.logger.info "============#{subject}======3================="
-		Rails.logger.info "============#{message}====4==================="
-		Rails.logger.info "============================5============"
 		mail(to: user_email, body: message, subject: subject, content_type: "text/html")
 	end
 end
