d82b76f4bb1c6fc9632b9f5f0b0e19a33977f03f
imtiaj2018 <imtiajromi@gmail.com>
2021-02-17 23:47:37 +0530

    mailgun implementation2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ApplicationMailer
 	default from: 'sunshineadsolutions@gmail.com'
 	
 	def send_mail(user_email, subject, message) 
-		Rails.logger.info "==================================="
-		Rails.logger.info "============#{user_email}======================="
-		Rails.logger.info "============#{subject}======================="
-		Rails.logger.info "============#{message}======================="
-		Rails.logger.info "========================================"
+		Rails.logger.info "========================1==========="
+		Rails.logger.info "============#{user_email}======2================="
+		Rails.logger.info "============#{subject}======3================="
+		Rails.logger.info "============#{message}====4==================="
+		Rails.logger.info "============================5============"
 		mail(to: user_email, body: message, subject: subject, content_type: "text/html")
 	end
 end
