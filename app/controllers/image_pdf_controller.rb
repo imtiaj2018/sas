d1b5b1fdf009ce05e660a6de1485b706f1b0aac0
imtiaj2018 <imtiajromi@gmail.com>
2023-01-22 01:52:43 +0530

    23

--- app/controllers/image_pdf_controller.rb
+++ app/controllers/image_pdf_controller.rb
@@ -112,7 +112,8 @@ class ImagePdfController < ApplicationController
 	def upload_project_images_form
 		time = Time.now().strftime("%m_%d_%Y_%I_%M_%S%p")
 		# directory="#{Rails.root}/project_images"
-		directory="/mnt/project_images"
+		# directory="/mnt/project_images"
+		directory="/public/project_images"
 		if !(File.directory? directory) 	#if directory is not present then creating
 			FileUtils.mkdir_p directory, :mode => 0777	rescue nil
 		end
