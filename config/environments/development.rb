ea8e03aea07591feb279608cf8e16f802c95853b
imtiaj2018 <imtiajromi@gmail.com>
2023-01-24 19:53:05 +0530

    sas update 2023

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  config.action_mailer.default_url_options = { host: 'http://194.195.116.203/sas' }
+	config.active_record.dump_schema_after_migration = false
+	config.action_mailer.default_url_options = { host: 'http://www.sunshineadsolutions.com' }
 	config.action_mailer.perform_deliveries = true
 	config.action_mailer.delivery_method = :smtp 
 	config.action_mailer.raise_delivery_errors = true 
