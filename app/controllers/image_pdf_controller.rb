ef9fbb2817993ecee08ffc876df40bddaa707b02
imtiaj2018 <imtiajromi@gmail.com>
2023-04-20 11:48:28 +0530

    image path added

--- app/controllers/image_pdf_controller.rb
+++ app/controllers/image_pdf_controller.rb
@@ -124,8 +124,11 @@ class ImagePdfController < ApplicationController
 		new_file_name = "#{file_name}_#{time}#{file_extention}"
 		final_file_name="#{directory}/#{new_file_name}" 
 		
+		puts "final_file_name===>>#{final_file_name}"
+		
 		FileUtils.move params[:upload]['datafile'].path, final_file_name  
-		ProjectImage.save_project_image_file(new_file_name,name,image_size) 
+		# ProjectImage.save_project_image_file(new_file_name,name,image_size) 
+		ProjectImage.save_project_image_file(new_file_name,name,image_size,final_file_name) 
 		session[:document_upload_status]="Uploaded Successfully"
 		redirect_to '/upload_project_images'		
 	end
