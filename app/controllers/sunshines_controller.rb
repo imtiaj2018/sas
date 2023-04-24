41b70cd1d8ee6ddb7001a6312bf2c44affe8d3b5
imtiaj2018 <imtiajromi@gmail.com>
2023-04-24 18:34:22 +0530

    image testing

--- app/controllers/sunshines_controller.rb
+++ app/controllers/sunshines_controller.rb
@@ -62,7 +62,8 @@ class SunshinesController < ApplicationController
 		UserMailer.send_mail(recipient_mail, recipient_subject, recipient_message).deliver #send to the visiter
 		# UserMailer.send_mail(sunshine_mail, sunshine_subject, sunshine_message).deliver #send to sunshine official mail id
 		
-		render :plain => "Thank you for contact. We will get back soon."
+		# render :plain => "Thank you for contact. We will get back soon."
+		render :text => "<script>alert(Thank you for contact. We will get back soon.)</script>"
 	end
 	
 	def admin_panel
