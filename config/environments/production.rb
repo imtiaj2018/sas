d59af753268b160c24cca7028325b36d3693d4f3
imtiaj2018 <imtiajromi@gmail.com>
2023-04-24 17:49:34 +0530

    email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,12 +96,12 @@ Rails.application.configure do
         config.action_mailer.delivery_method = :smtp
         config.action_mailer.raise_delivery_errors = true
         config.action_mailer.smtp_settings = {
-                :user_name => "imtiajromi@gmail.com",
-                :password => "Romi786@",
+                :user_name => "sunshineadsolutions@gmail.com",
+                :password => "lsnzgdlgsrrviyjf",
                 :domain => "gmail.com",
                 :address => "smtp.gmail.com",
                 :port => "587",
-                :authentication => :login,
+                :authentication => :plain,
                 :enable_starttls_auto => true
         }
 
