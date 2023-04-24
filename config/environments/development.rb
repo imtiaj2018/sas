275f94e052ea03a16c4411050a740ca527c85817
imtiaj2018 <imtiajromi@gmail.com>
2023-04-24 17:14:17 +0530

    email config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 	config.action_mailer.raise_delivery_errors = true 
 	config.action_mailer.smtp_settings = {
 		:user_name => "imtiajromi@gmail.com",
-		:password => "Romi786@",
+		:password => "zovtoosjddinkczi",
 		:domain => "gmail.com",
 		:address => "smtp.gmail.com",
 		:port => "587",
