533f9daa0957b275c91be195f4b1ebf6cb7924ee
imtiaj2018 <imtiajromi@gmail.com>
2023-04-24 19:20:03 +0530

    client image upload

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -10,7 +10,8 @@ class ClientsController < ApplicationController
 	
 	def upload_client_images_form
 		time = Time.now().strftime("%m_%d_%Y_%I_%M_%S%p")  
-		directory="#{Rails.root}/public/client_images"  
+		# directory="#{Rails.root}/public/client_images" 
+		directory="/mnt/client_images"
 		if !(File.directory? directory) 	#if directory is not present then creating
 			FileUtils.mkdir_p directory, :mode => 0777	rescue nil
 		end
@@ -19,10 +20,10 @@ class ClientsController < ApplicationController
 		file_extention = File.extname(name)
 		file_name = name.split(file_extention)[0]
 		new_file_name = "#{file_name}_#{time}#{file_extention}"
-		final_file_name="#{directory}/#{new_file_name}" 
+		final_file_name="#{directory}/#{new_file_name}"  #image full path
 		
 		FileUtils.move params[:upload]['datafile'].path, final_file_name  
-		Client.save_client_logo_file(new_file_name,name,client_name) 
+		Client.save_client_logo_file(new_file_name,name,client_name,final_file_name) 
 		session[:document_upload_status]="Uploaded Successfully"
 		redirect_to '/upload_clients'		
 	end
--- app/models/client.rb
+++ app/models/client.rb
@@ -8,11 +8,12 @@ class Client < ApplicationRecord
 		return Client.find_by_sql("SELECT count(*) as cc FROM clients;")
 	end
 	
-	def self.save_client_logo_file(file_name,display_name,client_name)
+	def self.save_client_logo_file(file_name,display_name,client_name,image_path)
 		doc_obj=Client.new
 		doc_obj.file_name = file_name
 		doc_obj.display_name = display_name
 		doc_obj.client_name = client_name
+		doc_obj.image_path = image_path
 		doc_obj.save
 	end
 	
@@ -22,5 +23,6 @@ class Client < ApplicationRecord
 	
 	def self.get_client_images
 		return Client.find_by_sql("select * from clients order by display_name").collect{|x| x.file_name}
+		# return Client.find_by_sql("select * from clients order by display_name").collect{|x| x.image_path}
 	end
 end
