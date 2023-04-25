5657f333495b705a05bb0658e1df6e0ea9184fe9
imtiaj2018 <imtiajromi@gmail.com>
2023-04-25 16:44:48 +0530

    mail ack message

--- app/controllers/sunshines_controller.rb
+++ app/controllers/sunshines_controller.rb
@@ -62,9 +62,9 @@ class SunshinesController < ApplicationController
 		UserMailer.send_mail(recipient_mail, recipient_subject, recipient_message).deliver #send to the visiter
 		UserMailer.send_mail(sunshine_mail, sunshine_subject, sunshine_message).deliver #send to sunshine official mail id
 		
-		render :plain => "Thank you for contact. We will get back soon."
+		# render :plain => "Thank you for contact. We will get back soon."
 		# render :plain => "<script>alert(Thank you for contact. We will get back soon.)</script>"
-		# render(html: "<script>alert('Thank you for contact. We will get back soon.')</script>".html_safe)
+		render(html: "<script>alert('Thank you for contact. We will get back soon.')</script>".html_safe)
 	end
 	
 	def admin_panel
