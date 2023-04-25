eff901166c27e08dbc2556d668bcb4de855a75f7
imtiaj2018 <imtiajromi@gmail.com>
2023-04-25 12:57:43 +0530

    mail ack message

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 	config.action_mailer.raise_delivery_errors = true 
 	config.action_mailer.smtp_settings = {
 		:user_name => "imtiajromi@gmail.com",
-		:password => "zovtoosjddinkczi",
+		:password => "Romi2023@apr",
 		:domain => "gmail.com",
 		:address => "smtp.gmail.com",
 		:port => "587",
