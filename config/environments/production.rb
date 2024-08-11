ccecbffa203bf39c4db29d58365a1a40107fa7f4
root <root@srv575542.hstgr.cloud>
2024-08-11 18:25:45 +0000

    all updated as sas_2023

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+	config.hosts << "www.sunshineadsolutions.com"
 	# Settings specified here will take precedence over those in config/application.rb.
 
 	# Code is not reloaded between requests.
